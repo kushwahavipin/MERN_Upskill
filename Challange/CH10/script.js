38ac5f56739962fc4afed22fbb5cbe929754e3a8
VIPIN KUSHWAHA <kushwahavipin786@gmail.com>
2024-06-18 19:20:10 +0530

    Challanges are completed

--- Challange/CH10/script.js
+++ Challange/CH10/script.js
@@ -1,17 +1,5 @@
-const image = document.querySelectorAll("img");
-function slide() {
-  image.forEach((el) => {
-    const slideAt = window.scrollY + window.innerHeight + el.height /2;
-    const imageBottom = el.offsetTop + el.height;
-    const isHalfShown = slideAt > el.offsetTop;
-    const isNotScrolledPast = window.scrollY < imageBottom;
-    if (isHalfShown && isNotScrolledPast) {
-      el.classList.add("active");
-    } else {
-      el.classList.remove("active");
-    }
-  });
-}
-
-slide();
-window.addEventListener("scroll", slide);
+let section = document.querySelector("section");
+window.addEventListener("scroll", () => {
+  let val = window.scrollY;
+  section.style.clipPath = `circle(${val * 2.3}px at left center)`;
+});
