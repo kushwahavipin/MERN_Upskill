a0a10405b8ae5cbcf3451ec526d7c9fbf54a64e6
VIPIN KUSHWAHA <kushwahavipin786@gmail.com>
2024-06-28 11:04:25 +0530

    Hero Section Created

--- Projects/our_portfolio/src/App.js
+++ Projects/our_portfolio/src/App.js
@@ -2,14 +2,29 @@ import styled, { ThemeProvider } from "styled-components";
 import { darkTheme } from "./utils/Themes";
 import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/sections/Hero";
 
-const App = () => {
+
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  width: 100%;
+  height: 100vh;
+  overflow-x: hidden;
+  position: relative;
+`;
+
+
+function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <BrowserRouter>
-      <Navbar/>
+        <Navbar />
+        <Body>
+          <Hero/>
+        </Body>
       </BrowserRouter>
-    </ThemeProvider>  
+    </ThemeProvider>
   );
 }
 
